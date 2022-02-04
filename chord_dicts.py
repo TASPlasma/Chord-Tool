4b62508fde75c527d761aeae98ed64ea082504f2
Derek <jazzfusion10@gmail.com>
2022-02-04 03:13:25 -0800

    Same as previous, but for real this time

---
+++ chord_dicts.py
@@ -0,0 +1,62 @@
+root_dict = {
+0: 'C',
+1: 'C#',
+2: 'D',
+3: 'Eb',
+4: 'E',
+5: 'F',
+6: 'F#',
+7: 'G',
+8: 'G#',
+9: 'A',
+10: 'Bb',
+11: 'B'
+}
+
+third_dict = {
+    0: 'sus2',
+    1: 'm',
+    2: 'maj',
+    3: 'sus4'
+}
+
+fifth_dict = {
+    0: ' b5',
+    1: u'\u266E5',
+    2: ' #5'
+}
+
+seventh_dict = {
+    0: 'no 7',
+    1: ' 7',
+    2: ' maj7'
+}
+
+ninth_dict = {
+    0: 'no 9',
+    1: ' b9',
+    2: u'\u266E9',
+    3: ' #9'
+}
+
+eleventh_dict = {
+    0: 'no 11',
+    1: u'\u266E11',
+    2: ' #11'
+}
+
+thirteenth_dict = {
+    0: 'no 13',
+    1: ' b13',
+    2: u'\u266E13'
+}
+
+chord_array_dict = {
+    0: root_dict,
+    1: third_dict,
+    2: fifth_dict,
+    3: seventh_dict,
+    4: ninth_dict,
+    5: eleventh_dict,
+    6: thirteenth_dict
+}
